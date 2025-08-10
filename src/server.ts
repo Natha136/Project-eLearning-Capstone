9fb60f46bc6a0306ee498e7f709cffc1bf43fe3c
Natha136 <nasywafebriani929@gmail.com>
2025-08-10 17:06:17 +0700

    Server

--- src/server.ts
+++ src/server.ts
@@ -10,12 +10,22 @@ const app = express();
 const upload = multer();
 const PORT = process.env.PORT || 3000;
 
+import { Request, Response } from 'express';
+
 // middleware request body, jika diperlukan
 app.use(upload.any());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/public', express.static(path.join(__dirname, '../public')));
+
+app.get('/', (req: Request, res: Response) => {
+  res.json({
+    message: 'Selamat datang di e-Learning!',
+    documentation: '/api',
+  });
+});
+
 // handle seluruh request /api/* ke route API
 app.use('/api', apiRoutes);
 
